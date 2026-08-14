2ca71809b51e747f1c37700c5ebaef5411409205
vinicius.castiglia <vinicius.castiglia@sou.inteli.edu.br>
2026-08-13 22:19:26 -0300

    chore(js-exercises): resolve exercícios do cap. 2.5

---
+++ javascript/jsinfo-2.5.js
@@ -0,0 +1,10 @@
+//Exercicio 2.6 js.info
+// What is te output of the script?
+
+let name = "Ilya";
+
+alert( `hello ${1}` ); // hello 1
+
+alert( `hello ${"name"}` ); // hello name
+
+alert( `hello ${name}` ); // hello Ilya
