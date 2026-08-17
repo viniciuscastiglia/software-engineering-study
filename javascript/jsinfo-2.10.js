75dedd535f9cb128de33865c8d424eb2c939d7c9
vinicius.castiglia <vinicius.castiglia@sou.inteli.edu.br>
2026-08-17 15:19:08 -0300

    add: jsinfo 2.10 exercicios

---
+++ javascript/jsinfo-2.10.js
@@ -0,0 +1,28 @@
+/*Rewrite 'if' into '?'
+Rewrite this if using the conditional operator '?':*/
+
+let result;
+
+if (a + b < 4) {
+  result = 'Below';
+} else {
+  result = 'Over';
+}
+
+let results = (a + b < 4) ? 'Below' : 'Over'
+
+let message;
+
+if (login == 'Employee') {
+  message = 'Hello';
+} else if (login == 'Director') {
+  message = 'Greetings';
+} else if (login == '') {
+  message = 'No login';
+} else {
+  message = '';
+}
+
+let messages = (login == 'Employee') ? 'Hello' :
+ (login == 'Director') ? 'Greetings' :
+ (login == '') ? 'No login' : ''
